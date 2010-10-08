53e7151680104ed9c8969c636bd534876d8d01aa
dylanvorster.dv@gmail.com <dylanvorster.dv@gmail.com@92af2f73-fcde-76f7-70b0-5f8a4a3b5d68>
2010-10-08 14:01:12 +0000

    Source files

--- SystemSimulator/src/ProgramGUI/GUIComponents/LogicControls/Slider.java
+++ SystemSimulator/src/ProgramGUI/GUIComponents/LogicControls/Slider.java
@@ -7,8 +7,6 @@ import java.awt.Color;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.event.ChangeEvent;
