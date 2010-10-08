3dbd4047c24c2d660d74c019d0268c5293fb04c1
shaunmichaelvantonder <shaunmichaelvantonder@92af2f73-fcde-76f7-70b0-5f8a4a3b5d68>
2010-10-08 17:57:03 +0000

    removed unneeded import

--- SystemSimulator/src/MainClasses/SystemInformation.java
+++ SystemSimulator/src/MainClasses/SystemInformation.java
@@ -7,7 +7,7 @@ public class SystemInformation {
     
     public static String buildCodeName = "EXE-NAMKCOR";
     public static double buildVersion = 0.01;
-    public static double SystemVersion = 1.0;
+    public static double SystemVersion = 0.2;
     public static double saveVersion = 1.0;
 
 }
